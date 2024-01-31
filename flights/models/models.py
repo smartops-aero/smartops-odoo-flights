d4d38c6e9f211bdaf80c403770bd0d77c2e50170
Ivan Kropotkin <yelizariev@itpp.dev>
2024-01-31 12:41:08 +0100

    add models

--- flights/models/models.py
+++ flights/models/models.py
@@ -1,18 +1,77 @@
-# -*- coding: utf-8 -*-
+# Copyright 2024 Ivan Kropotkin <https://twitter.com/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
 
-# from odoo import models, fields, api
+from odoo import fields, models
+
+# TODO: move the models to individual files
 
 
-# class flights(models.Model):
-#     _name = 'flights.flights'
-#     _description = 'flights.flights'
+class FlightAirport(models.Model):
+    _name = 'flight.airport'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    code = fields.Char()
+    partner_id = fields.Char("Address")
+
+
+class FlightAircraft(models.Model):
+    _name = 'flight.aircraft'
+
+    registration = fields.Char("Registration number")
+
+
+class FlightNumber(models.Model):
+    _name = 'flight.number'
+
+    airline_id = fields.Many2one('flight.airline')
+    prefix = fields.Char()
+    numbers = fields.Char()
+
+
+class FlightAirline(models.Model):
+    _name = 'flight.airline'
+
+    name = fields.Char()
+    default_prefix = fields.Char(help="Default prefix assigned on creating a new flight.number for this airline")
+
+
+class FlightCrew(models.Model):
+    _name = 'flight.crew'
+
+    partner_id = fields.Many2one('res.partner')
+    role_id = fields.Many2one('flight.crew.role')
+
+
+class FlightCrewRole(models.Model):
+    _name = 'flight.crew.role'
+
+    code = fields.Char()
+    description = fields.Char()
+
+
+class FlightFlight(models.Model):
+    _name = 'flight.flight'
+
+    aircraft_id = fields.Many2one('flight.aircraft')
+    flight_number_id = fields.Many2one('flight.number')
+    crew_ids = fields.One2many('flight.crew', 'flight_id')
+
+    departure_id = fields.Many2one('flight.airport')
+    event_ids = fields.One2many('flight.event', 'flight_id')
+    arrival_id = fields.Many2one('flight.airport')
+
+
+class FlightEvent(models.Model):
+    _name = 'flight.event'
+
+    event_type = fields.Many2one('flight.event.type')
+    actual_date = fields.Datetime()
+    estimated_date = fields.Datetime()
+    scheduled_date = fields.Datetime()
+    target_date = fields.Datetime()
+
+
+class FlightEventType(models.Model):
+    _name = 'flight.event.type'
+
+    code = fields.Char()
+    description = fields.Char()
--- flights_pilotlog/models/models.py
+++ flights_pilotlog/models/models.py
@@ -1,18 +1,46 @@
-# -*- coding: utf-8 -*-
+# Copyright 2024 Ivan Kropotkin <https://twitter.com/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
 
-# from odoo import models, fields, api
+from odoo import fields, models
+
+# TODO: move the models to individual files
 
 
-# class flights_pilotlog(models.Model):
-#     _name = 'flights_pilotlog.flights_pilotlog'
-#     _description = 'flights_pilotlog.flights_pilotlog'
+class FlightAircraft(models.Model):
+    _inherit = 'flight.aircraft'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    make_id = fields.Many2one("flight.aircraft.make")
+    model_id = fields.Many2one("flight.aircraft.model")
+
+
+class FlightFlight(models.Model):
+    _inherit = 'flight.flight'
+
+    period_ids = fields.One2many("flight.period", 'flight_id')
+
+
+class FlightAircraftMake(models.Model):
+    _name = 'flight.aircraft.make'
+
+    name = fields.Char()
+
+
+class FlightAircraftModel(models.Model):
+    _name = 'flight.aircraft.model'
+
+    name = fields.Char()
+
+
+class FlightPeriod(models.Model):
+    _name = 'flight.period'
+
+    flight_id = fields.Many2one('flight.flight')
+    period_type_id = fields.Many2one('flight.period.type')
+    duration = fields.Integer(help="Duration in minutes")
+    # TODO: check if we need more models
+
+
+class FlightPeriodType(models.Model):
+    _name = 'flight.period.type'
+
+    code = fields.Char()
