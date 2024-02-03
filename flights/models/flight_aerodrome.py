9377ade2bc474c7c7013e735a1d41dec4bab6e74
Alexis Yushin <alexis@apexive.com>
2024-02-03 09:46:48 -0400

    wip aerodrome model

---
+++ flights/models/flight_aerodrome.py
@@ -0,0 +1,56 @@
+# Copyright 2024 Apexive <https://apexive.com/>
+# License MIT (https://opensource.org/licenses/MIT).
+import json
+from odoo import models, fields
+
+
+class FlightAerodrome(models.Model):
+    _name = 'flight.aerodrome'
+    _inherit = 'flight.base'
+
+    partner_id = fields.Char("Address")
+
+    icao = fields.Char("ICAO identifier", unique=True, blank=True)
+    iata = fields.Char("IATA identifier", unique=True, blank=True)
+    elevation = fields.Integer("Aerodrome elevation in feet")
+
+    # TODO(ivank):
+    #  type =  enum("Airport", "Aerodrome", "Seabase", "Heliport")
+    # TODO(ivank): please ensure the importer saves all these fields in the partner_id
+    # NotesUser will be extender by flights_pilotlog module with FlightsAerodromePilotNotes
+    # {
+    #  "user_id": 125880,
+    #  "table": "Airfield",
+    #  "guid": "00000000-0000-0000-0000-000000040048",
+    #  "meta": {
+    #    "AFCat": 8,
+    #    "AFCode": "00000000-0000-0000-0000-000000040048",
+    #    "AFIATA": "NKT",
+    #    "AFICAO": "LTCV",
+    #    "AFName": "Sirnak Serafettin Elci",
+    #    "TZCode": 333,
+    #    "Latitude": 37218,
+    #    "ShowList": false,
+    #    "AFCountry": 223,
+    #    "Longitude": -42036,
+    #    "NotesUser": "ATIS 128.400",
+    #    "RegionUser": 0,
+    #    "ElevationFT": 0,
+    #    "Record_Modified": 1616320991
+    #  },
+    #  "platform": 9,
+    #  "_modified": 1616317613
+    # },
+
+    def _parse_pilot_log_mcc(self, flight_data):
+        data = json.loads(flight_data.raw_text)
+        meta = data.get("meta", {})
+        partner = self.env["res.partner"]._sync_flight_data(flight_data, {
+            # TODO: check mapping
+            "name": meta.get("AFName"),
+        })
+        return self._sync_flight_data(flight_data, {
+            # TODO: check mapping
+            "iata": meta.get("AFIATA"),
+            "partner_id": partner.id,
+        })
