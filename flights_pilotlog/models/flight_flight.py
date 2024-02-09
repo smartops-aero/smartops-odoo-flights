ceb1aba15c3f5840cf22b91e1acd3a961ad1e05d
Ivan Kropotkin <yelizariev@itpp.dev>
2024-02-09 18:44:05 +0100

    fix domain

--- flights_pilotlog/models/flight_flight.py
+++ flights_pilotlog/models/flight_flight.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class FlightFlight(models.Model):
     _inherit = 'flight.flight'
 
-    condition_time_ids = fields.One2many('flight.time', 'flight_id', domain="[('time_type_id.group', '=', 'condition')]")
-    pilot_time_ids = fields.One2many('flight.time', 'flight_id', domain="[('time_type_id.group', '=', 'function')]")
+    condition_time_ids = fields.One2many('flight.time', 'flight_id', domain=[('time_type_id.group', '=', 'condition')])
+    pilot_time_ids = fields.One2many('flight.time', 'flight_id', domain=[('time_type_id.group', '=', 'function')])
