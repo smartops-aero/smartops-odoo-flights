b488b82cb9e715aca31e424209c202b7aa467013
Ivan Kropotkin <yelizariev@itpp.dev>
2024-02-09 13:36:07 +0100

    add flight time types

--- flights_pilotlog/__manifest__.py
+++ flights_pilotlog/__manifest__.py
@@ -26,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/flight_time_type_data.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
