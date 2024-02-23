1ae7a82f4028e9e929757511ae7049c515986d78
Ivan Kropotkin <help@itpp.dev>
2024-02-23 13:27:22 +0100

    [ADD] flights_pilotlog: new computed fields flight_time, total_time

--- flights_pilotlog/models/flight_flight.py
+++ flights_pilotlog/models/flight_flight.py
@@ -1,9 +1,18 @@
 # Copyright 2024 Apexive <https://apexive.com/>
 # License MIT (https://opensource.org/licenses/MIT).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class FlightFlight(models.Model):
     _inherit = 'flight.flight'
 
     pilot_time_ids = fields.One2many('flight.time', 'flight_id')
+    flight_time = fields.Integer("Flight Time", compute='_compute_flight_time')
+    total_time = fields.Integer("Total Flight Time", compute='_compute_flight_time')
+
+    @api.depends('pilot_time_ids')
+    def _compute_flight_time(self):
+        for flight in self:
+            # TODO: check computation
+            flight.flight_time = sum(record.minutes for record in flight.pilot_time_ids if record.time_kind_id.name in ["DUAL", "PIC"])
+            flight.total_time = sum(record.minutes for record in flight.pilot_time_ids if record.time_kind_id.name == "TOTAL")
