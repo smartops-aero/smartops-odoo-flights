b9c7f55b149e0b1e1685d60d0382c05fa1dbb74e
Ivan Kropotkin <help@itpp.dev>
2024-03-01 19:33:59 +0100

    aerodrome data: add geo conversion of coordinates to time zone

--- flights/doc/aerodrome/airport_codes.py
+++ flights/doc/aerodrome/airport_codes.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+from timezonefinder import TimezoneFinder
 
 if len(sys.argv) != 2:
     print("Usage: {} <csv_file>".format(sys.argv[0]))
@@ -12,6 +13,8 @@ csv_file = sys.argv[1]
 # Output filenames
 partner_csv_filename = 'res.partner.csv'
 aerodrome_csv_filename = 'flight.aerodrome.csv'
+
+tf = TimezoneFinder()
 
 # Open the input CSV file
 with open(csv_file, newline='') as input_csvfile:
@@ -26,7 +29,7 @@ with open(csv_file, newline='') as input_csvfile:
         aerodrome_writer = csv.writer(aerodrome_csvfile, quoting=csv.QUOTE_ALL)
 
         # Write headers to both files
-        partner_writer.writerow(['id', 'name', 'city', 'country_id/id', "partner_latitude", "partner_longitude"])
+        partner_writer.writerow(['id', 'name', 'city', 'country_id/id', "partner_latitude", "partner_longitude", "tz"])
         aerodrome_writer.writerow(['id', 'partner_id/id', 'icao', 'iata', 'elevation', 'aerodrome_type'])
 
         # Skip the header line in the input CSV file
@@ -36,18 +39,21 @@ with open(csv_file, newline='') as input_csvfile:
         for row in reader:
             ident, type, name, elevation_ft, continent, iso_country, iso_region, municipality, gps_code, iata_code, local_code, coordinates = row
 
-            partner_latitude, partner_longitude = None, None
+            partner_latitude, partner_longitude = 0, 0
+            tz = "UTC"
             if coordinates:
-                partner_latitude, partner_longitude = coordinates.split(", ")
+                partner_longitude, partner_latitude = coordinates.split(", ")
+                partner_longitude = float(partner_longitude)
+                partner_latitude = float(partner_latitude)
+                tz = tf.certain_timezone_at(lat=float(partner_latitude), lng=partner_longitude)
 
             # Fix country code
             iso_country = iso_country.lower()
             if iso_country == "gb":
                 iso_country = "uk"
 
-            # TODO: compute tz (from coordinates or country + municipality)
             # Write data to res.partner.csv
-            partner_writer.writerow(['partner_' + ident, name, municipality, 'base.' + iso_country, partner_latitude, partner_longitude])
+            partner_writer.writerow(['partner_' + ident, name, municipality, 'base.' + iso_country, partner_latitude, partner_longitude, tz])
 
             # Write data to flights.aerodrome.csv
             aerodrome_writer.writerow(['aerodrome_' + ident, 'partner_' + ident, ident, iata_code, elevation_ft, type])
