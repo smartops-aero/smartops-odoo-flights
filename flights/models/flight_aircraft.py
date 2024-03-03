16eacbac2de133f021123c7147c58d2f3bca09a3
Alexis Yushin <alexis@apexive.com>
2024-03-03 12:49:33 -0400

    aircraft table tweaks

--- flights/models/flight_aircraft.py
+++ flights/models/flight_aircraft.py
@@ -3,6 +3,23 @@
 from odoo import models, fields
 
 
+class FlightAircraftClass(models.Model):
+    _name = 'flight.aircraft.class'
+    _description = 'Aircraft Category and Class'
+
+    aircraft_category = fields.Selection([
+        ('airplane', 'Airplane'),
+        ('rotorcraft', 'Rotorcraft'),
+        ('glider', 'Glider'),
+        ('lighter_than_air', 'Lighter than Air'),
+        ('powered_lift', 'Powered Lift'),
+        ('powered_parachute', 'Powered Parachute'),
+        ('weight_shift_control', 'Weight Shift Control')
+    ], string='Aircraft Category', help='Select the category of the aircraft')
+
+    name = fields.Char("Aircraft class")
+
+
 class FlightAircraftMake(models.Model):
     _name = 'flight.aircraft.make'
     _description = 'Aircraft Make'
@@ -28,7 +45,33 @@ class FlightAircraftModel(models.Model):
 
     name = fields.Char()
     make_id = fields.Many2one("flight.aircraft.make")
+    class_id = fields.Many2one("flight.aircraft.class")
+
+    engine_type = fields.Selection([
+        ('diesel', 'Diesel'),
+        ('electric', 'Electric'),
+        ('non_powered', 'Non-Powered'),
+        ('piston', 'Piston'),
+        ('radial', 'Radial'),
+        ('turbofan', 'Turbofan'),
+        ('turbojet', 'Turbojet'),
+        ('turboprop', 'Turboprop'),
+        ('turboshaft', 'Turboshaft')
+    ], string='Engine Type', help='Select the engine type of the aircraft')
+
+    gear_type = fields.Selection([
+        ('amphibian', 'Amphibian (AM)'),
+        ('fixed_tailwheel', 'Fixed Tailwheel (FC)'),
+        ('fixed_tricycle', 'Fixed Tricycle (FT)'),
+        ('floats', 'Floats (FL)'),
+        ('retractable_tailwheel', 'Retractable Tailwheel (RC)'),
+        ('retractable_tricycle', 'Retractable Tricycle (RT)'),
+        ('skids', 'Skids'),
+        ('skis', 'Skis')
+    ], string='Gear Type', help='Select the gear type of the aircraft')
+
     code = fields.Char("ICAO type code")
+
     tag_ids = fields.Many2many("flight.aircraft.model.tag")
 
     #  if   "Kg5700": is true - record 142000 lbs by defaukt as mtow (medium), otherwise record 12000 lbs by default (light)
@@ -43,11 +86,6 @@ class FlightAircraftModel(models.Model):
         return "L"
 
 
-    # TODO(ivank): add class and category models as per below
-    # class_id = fields.Many2many()  # TODO: why not Selection / Integer ?
-    # category_id = fields.Many2many() # TODO: why not Selection ?
-
-
 class FlightAircraft(models.Model):
     _name = 'flight.aircraft'
     _inherit = 'flight.base'
@@ -60,6 +98,15 @@ class FlightAircraft(models.Model):
     year = fields.Date("Year of manufacture")
 
     model_id = fields.Many2one("flight.aircraft.model")
+
     _sql_constraints = [
         ("registration_unique", "unique(registration)", "Aircraft with this registration number already exists!")
     ]
+
+    equipment_type = fields.Selection([
+        ('aircraft', 'Aircraft'),
+        ('ffs', 'Full Flight Simulator (FFS)'),
+        ('ftd', 'Flight Training Device (FTD)'),
+        ('batd', 'Basic Aircraft Training Device (BATD)'),
+        ('aatd', 'Advanced Aircraft Training Device (AATD)'),
+    ], string='Equipment Type', default='aircraft')
